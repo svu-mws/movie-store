0b35b438ad03166695421459ddcbd03ca6e2d1c2
mohammadnashouq <mohammadnashouq@gmail.com>
2019-06-02 02:41:31 +0300

    Update admincontroller.php

--- app/Http/Controllers/admincontroller.php
+++ app/Http/Controllers/admincontroller.php
@@ -13,6 +13,7 @@ use \App\actor;
 use \App\type;
 use \App\Movies_order;
 use Auth;
+ use App\Http\Resources\movie as movie1 ;
 
 class admincontroller extends Controller
 {
@@ -21,27 +22,36 @@ class admincontroller extends Controller
     //
     Function dashboard()
     {
-      return view('admin.Dashboard');
+        return (new movie1(['show admin.Dashboard']))
+            ->response()
+            ->setStatusCode(200);
+      // return view('admin.Dashboard');
     }
 
     Function comments()
           {
             $comments = Comment::all();
-            return View('admin.comments',compact('comments'));
+            // return View('admin.comments',compact('comments'));
+              return  movie1::collection($comments);
           }
           Function movies()
           {
             $movies = Movie::all();
-            return View('admin.movies',compact('movies'));
+            // return View('admin.movies',compact('movies'));
+              return  movie1::collection($movies);
           }
           Function users()
           {
             $users=User::all();
-            return View('admin.users',compact('users'));
+            // return View('admin.users',compact('users'));
+              return  movie1::collection($users);
           }
           public function uploadnewmovie()
   {
-    return view('admin.uploadnewmovie');
+    // return view('admin.uploadnewmovie');
+      return  (new movie1(['show upload movie page']))
+            ->response()
+            ->setStatusCode(200);
   }
   public function uploadnewmovie1(request $request)
 {
@@ -66,17 +76,27 @@ $movie->thumbnail =$request['thumbnail'];
 
   $movie->prodicer_id = $request['prodicer_id'];
     $movie->save();
-    return back()->with('success' , 'movie is successfully created');
+
+     return (new movie1(['movie has successfully uploaded']))
+            ->response()
+            ->setStatusCode(201);
 }
 public function deletemovie($id)
     {
         $movie = Movie::where('id', $id)->first()->delete();
-          return back();
+          // return back();
+          return (new movie1(['movie is successfully deleted']))
+                 ->response()
+                 ->setStatusCode(202);
     }
     public function editmovie($id)
       {
         $movie = Movie::where('id', $id)->first();
-        return view('admin.editMovie',compact('movie'));
+        movie1::withoutWrapping();
+        return (new movie1($movie))
+        ->response()
+        ->setStatusCode(200);
+        // return view('admin.editMovie',compact('movie'));
       }
       public function editmovie1(CreatMovie $request,$id)
       {
@@ -86,16 +106,30 @@ public function deletemovie($id)
         $movie->thumbnail = $request['thumbnail'];
 
           $movie->save();
-          return back()->with('success' , 'Movie is successfully updated');
+          movie1::withoutWrapping();
+          return (new movie1($movie))
+          ->response()
+          ->setStatusCode(202);
+          // return back()->with('success' , 'Movie is successfully updated');
       }
 
 
       Function actors()
       {
         $actors = Actor::all();
-        return View('admin.actors',compact('actors'));
+        // return View('admin.actors',compact('actors'));
+        return  movie1::collection($actors)
+        ->response()
+          ->setStatusCode(204);
       }
-      public function addnewactor(request $request)
+      public function addnewactor()
+      {
+        movie1::withoutWrapping();
+        return (new movie1(['show add actor page']))
+        ->response()
+          ->setStatusCode(204);
+      }
+      public function addnewactor1(request $request)
     {
       $this->validate($request,[
     'name'=>'required|string',
@@ -117,17 +151,28 @@ public function deletemovie($id)
 
 
         $actor->save();
-        return back()->with('success' , 'movie is successfully created');
+        return (new movie1(['movie is successfully created']))
+        ->response()
+          ->setStatusCode(201);
+        // return back()->with('success' , 'movie is successfully created');
     }
     public function deleteactor($id)
         {
             $actor = Actor::where('id', $id)->first()->delete();
-              return back();
+            return (new movie1(['Actor is successfully deleted']))
+            ->response()
+              ->setStatusCode(202);
+              // return back();
         }
         public function editactor($id)
           {
             $actor = Actor::where('id', $id)->first();
-            return view('admin.editactor',compact('actor'));
+            movie1::withoutWrapping();
+
+            return (new movie1($actor))
+            ->response()
+            ->setStatusCode(200);
+            // return view('admin.editactor',compact('actor'));
           }
           public function editactor1(CreatMovie $request,$id)
           {
@@ -137,16 +182,26 @@ public function deletemovie($id)
             $actor->thumbnail = $request['thumbnail'];
 
               $actor->save();
-              return back()->with('success' , 'Movie is successfully updated');
+              movie1::withoutWrapping();
+
+              return (new movie1($actor))
+              ->response()
+              ->setStatusCode(202);
+              // return back()->with('success' , 'Movie is successfully updated');
           }
 
 
           Function producers()
           {
             $producers = producer::all();
-            return View('admin.producers',compact('producers'));
+            return movie1::collection($producers);
+            // return View('admin.producers',compact('producers'));
           }
-          public function addnewproducer(request $request)
+          public function addnewproducer()
+        {
+          return new movie1(['show add new producer page']);
+        }
+          public function addnewproducer1(request $request)
         {
           $this->validate($request,[
         'name'=>'required|string',
@@ -168,17 +223,30 @@ public function deletemovie($id)
 
 
             $producer->save();
-            return back()->with('success' , 'movie is successfully created');
+            movie1::withoutWrapping();
+
+            return (new movie1($producer))
+            ->response()
+            ->setStatusCode(201);
+            // return back()->with('success' , 'movie is successfully created');
         }
         public function deleteproducer($id)
             {
                 $producer = Producer::where('id', $id)->first()->delete();
-                  return back();
+                return (new movie1(['producer has been deleted successfully']))
+                ->response()
+                ->setStatusCode(202);
+                  // return back();
             }
             public function editproducer($id)
               {
                 $producer = producer::where('id', $id)->first();
-                return view('admin.editproducer',compact('producer'));
+                movie1::withoutWrapping();
+
+                return (new movie1($producer))
+                ->response()
+                ->setStatusCode(202);
+                // return view('admin.editproducer',compact('producer'));
               }
               public function editproducer1(CreatMovie $request,$id)
               {
@@ -188,15 +256,26 @@ public function deletemovie($id)
                 $producer->thumbnail = $request['thumbnail'];
 
                   $actor->save();
-                  return back()->with('success' , 'Movie is successfully updated');
+                  movie1::withoutWrapping();
+                  return (new movie1($actor))
+                  ->response()
+                  ->setStatusCode(202);
+                  // return back()->with('success' , 'Movie is successfully updated');
               }
 
               Function moviestypes()
               {
                 $moviestypes = Type::all();
-                return View('admin.moviestypes',compact('moviestypes'));
+                return  movie1::collection($moviestypes);
+                // return View('admin.moviestypes',compact('moviestypes'));
               }
-              public function addnewmoviestype(request $request)
+              public function addnewmoviestype()
+              {
+                return (new movie1(['show add type page']))
+                ->response()
+                ->setStatusCode(200);
+              }
+              public function addnewmoviestype1(request $request)
             {
               $this->validate($request,[
             'name'=>'required|string',
@@ -217,30 +296,47 @@ public function deletemovie($id)
 
 
 
-                $producer->save();
-                return back()->with('success' , 'movie is successfully created');
+                $moviestypes->save();
+                movie1::withoutWrapping();
+
+                return (new movie1($moviestypes))
+                ->response()
+                ->setStatusCode(201);
+                // return back()->with('success' , 'movie is successfully created');
             }
             public function deletemoviestype($id)
                 {
                     $moviestypes = Type::where('id', $id)->first()->delete();
-                      return back();
+                    movie1::withoutWrapping();
+                      return (new movie1(['']))
+                      ->response()
+                      ->setStatusCode(202);
                 }
                 public function editmoviestype($id)
                   {
                     $moviestypes = Type::where('id', $id)->first();
-                    return view('admin.editmoviestype',compact('moviestypes'));
+                    movie1::withoutWrapping();
+                    return (new movie1($moviestypes))
+                    ->response()
+                    ->setStatusCode(202);
+                    // return view('admin.editmoviestype',compact('moviestypes'));
                   }
                   public function editmoviestype1(CreatMovie $request,$id)
                   {
                     $moviestypes = Type::where('id', $id)->first();
                     $moviestypes->name = $request['name'];
                       $actor->save();
-                      return back()->with('success' , 'Movie is successfully updated');
+                      movie1::withoutWrapping();
+                      return (new movie1($moviestypes))
+                      ->response()
+                      ->setStatusCode(202);
+                      // return back()->with('success' , 'Movie is successfully updated');
                   }
 
 public function reports()
 {
-  return view('admin.report') ;
+  return new movie1('[show report page]');
+  // return view('admin.report') ;
 }
 public function reports1(request $request)
 {
@@ -256,6 +352,7 @@ public function reports1(request $request)
 
     $Downloaded_movies = Movies_order::whereBetween('created_at', [$startdate, $enddate])->get();
 
-return view('admin.generatedreport',compact('Downloaded_movies'));
+// return view('admin.generatedreport',compact('Downloaded_movies'));
+return movie1::collection($Downloaded_movies);
 }
 }
